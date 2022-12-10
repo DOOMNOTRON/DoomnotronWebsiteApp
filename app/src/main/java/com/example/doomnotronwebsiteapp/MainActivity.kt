066192c6f1018290eea4adb56974d9b4562ed2a7
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-12-10 04:41:06 -0800

    Add permission to use the internet.

--- app/src/main/java/com/example/doomnotronwebsiteapp/MainActivity.kt
+++ app/src/main/java/com/example/doomnotronwebsiteapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.doomnotronwebsiteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Loads the url for the webView
+        val mWebView = findViewById<View>(R.id.WebView) as WebView
+        mWebView.loadUrl("https://doomnotron.azurewebsites.net/")
+
+        // Enable Javascript
+        val webSettings = mWebView.settings
+        webSettings.javaScriptEnabled = true
+        mWebView.webViewClient = WebViewClient()
+
+        mWebView.canGoBack()
+        mWebView.setOnKeyListener(View.OnKeyListener { view, i, keyEvent ->
+            if (i == KeyEvent.KEYCODE_BACK
+                && keyEvent.action == MotionEvent.ACTION_UP
+                && mWebView.canGoBack()) {
+                mWebView.goBack()
+                return@OnKeyListener true
+            }
+            false
+        })
     }
 }

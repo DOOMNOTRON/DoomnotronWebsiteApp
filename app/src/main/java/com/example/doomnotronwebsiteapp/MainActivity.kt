e5458d361f6eb29777a14e0899338e4b5e820546
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-12-13 07:30:57 -0800

    Added touch and multi-touch functionality. I also added log events to demonstrate how the user is touching the screen. IE one finger or multiple fingers.

--- app/src/main/java/com/example/doomnotronwebsiteapp/MainActivity.kt
+++ app/src/main/java/com/example/doomnotronwebsiteapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.doomnotronwebsiteapp
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
@@ -33,6 +35,24 @@ class MainActivity : AppCompatActivity() {
         mWebView.getSettings().setSupportZoom(true)
         mWebView.getSettings().setBuiltInZoomControls(true) // allow pinch to zoom
         mWebView.getSettings().setDisplayZoomControls(false) // disable the default zoom controls on the page
+
+        // gets the motion events and creates log depending on the users actions
+        mWebView.setOnTouchListener {v: View, m: MotionEvent ->
+            val eventType = m.getActionMasked()
+            when (eventType) {
+                // one finger touches the screen
+                MotionEvent.ACTION_DOWN -> Log.d(TAG, "TouchEvents: Action down")
+                // one finger lifts from the screen
+                MotionEvent.ACTION_UP -> Log.d(TAG, "TouchEvents: Action Up")
+                // multiple fingers touches the screen
+                MotionEvent.ACTION_POINTER_DOWN -> Log.d(TAG, "TouchEvents: Action Pointer Down")
+                // multiple fingers lift from the screen
+                MotionEvent.ACTION_POINTER_UP -> Log.d(TAG, "TouchEvents: Action Pointer Up")
+                // user moves finger on the screen
+                MotionEvent.ACTION_MOVE -> Log.d(TAG, "TouchEvents: Action Move")
+            }
+            false
+        }
 
         // enable the go back button
         mWebView.canGoBack()
